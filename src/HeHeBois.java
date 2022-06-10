ce7380c0a5f50475b44b659b8a4d8eed274816a0
DarKNooB0 <vongolabookrock@gmail.com>
2022-06-10 11:51:46 -0700

    two commit

--- src/HeHeBois.java
+++ src/HeHeBois.java
@@ -1,8 +1,14 @@
 public class HeHeBois {
     public static void main(String[] args) {
-
+        int a;
+        int b;
+        int sum;
+        a = 100;
+        b = 3;
+        sum = a*b;
         System.out.println("sa wad dee kub p shy!!!");
+        System.out.println("eiei");
         System.out.println((100+200+300-300)/5);
-
+        System.out.println(sum);
     }
 }
