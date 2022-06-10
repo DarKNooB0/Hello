4f3e052bc8459e4f869bf5e8f2d30452bc86891f
DarKNooB0 <vongolabookrock@gmail.com>
2022-06-10 11:38:02 -0700

    frist commit

---
+++ src/HeHeBois.java
@@ -0,0 +1,8 @@
+public class HeHeBois {
+    public static void main(String[] args) {
+
+        System.out.println("sa wad dee kub p shy!!!");
+        System.out.println((100+200+300-300)/5);
+
+    }
+}
